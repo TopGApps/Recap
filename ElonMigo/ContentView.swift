5fbcf5cd056461e75999c362e4ea662e48366fe5
Aaron <0d1y6g0w@duck.com>
2024-05-25 08:59:05 -0700

    Inital commit

---
+++ ElonMigo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElonMigo
+//
+//  Created by Aaron Ma on 5/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElonMigo/ElonMigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElonMigoApp.swift
+//  ElonMigo
+//
+//  Created by Aaron Ma on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ElonMigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
