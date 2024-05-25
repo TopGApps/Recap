4713a70b3822014d45c1d16138159e468f171f09
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2024-05-25 12:25:34 -0700

    Changes to ContentView UI for home view

--- ElonMigo/ContentView.swift
+++ ElonMigo/ContentView.swift
@@ -15,9 +15,13 @@ struct ContentView: View {
     
     @State private var userInput = ""
     
+    // Settings
+    @AppStorage("model") private var selectedOption = "gemini-1.5-pro-latest"
+    let options = ["Gemini 1.5 Pro", "Gemini 1.5 Flash"]
+    
     // Web Search
     @State private var showingURLSheet = false
-    @State private var links = [""]
+    @State private var links: [String] = []
     
     // Photos Picker
     @State private var selectedItems: [PhotosPickerItem] = []
@@ -30,52 +34,54 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                HStack {
-                    PhotosPicker(selection: $selectedItems, maxSelectionCount: 1, matching: .images) {
-                        Label("Select Photos", systemImage: "photo.on.rectangle.angled")
-                    }
-                }
-                .buttonStyle(.bordered)
-                .onChange(of: selectedItems) { items in
-                    selectedPhotosData = []
-                    
-                    for i in items {
-                        Task {
-                            if let data = try? await i.loadTransferable(type: Data.self) {
-                                selectedPhotosData.append(data)
-                            }
-                        }
-                    }
-                }
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(selectedPhotosData, id: \.self) { photoData in
-                            if let image = UIImage(data: photoData) {
-                                Image(uiImage: image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .cornerRadius(10.0)
-                            }
-                        }
-                    }
-                }
-                
-                Button {
-                    showingURLSheet = true
-                } label: {
-                    Label("Web Search", systemImage: "magnifyingglass")
-                }
-                .buttonStyle(.bordered)
-                
-                Divider()
-                
                 TextField("What would you like to study?", text: $userInput)
                     .padding()
                     .clipShape(RoundedRectangle(cornerRadius: 15))
                     .overlay(RoundedRectangle(cornerRadius: 15).stroke(.gray, lineWidth: 1))
                     .scrollDismissesKeyboard(.interactively)
                     .padding(.horizontal)
+                HStack {
+                    Spacer()
+                    HStack {
+                        PhotosPicker(selection: $selectedItems, maxSelectionCount: 1, matching: .images) {
+                            if selectedItems.count == 1 {
+                                Label("\(selectedItems.count != 0 ? "\(selectedItems.count) Selected" : "")", systemImage: "photo")
+                            } else if selectedItems.count == 0 {
+                                Image(systemName: "photo")
+                            } else {
+                                Label("\(selectedItems.count != 0 ? "\(selectedItems.count) Selected" : "")", systemImage: "photo")
+                            }
+                        }
+                    }
+                    .buttonStyle(.bordered)
+                    .clipShape(RoundedRectangle(cornerRadius: 100))
+                    .onChange(of: selectedItems) { items in
+                        selectedPhotosData = []
+                        
+                        for i in items {
+                            Task {
+                                if let data = try? await i.loadTransferable(type: Data.self) {
+                                    selectedPhotosData.append(data)
+                                }
+                            }
+                        }
+                    }
+                    
+                    Button {
+                        showingURLSheet = true
+                    } label: {
+                        if links.count == 1 {
+                            Label("\(links.count != 0 ? "\(links.count) Link" : "")", systemImage: "link.badge.plus")
+                        } else if links.count == 0 {
+                            Image(systemName: "link.badge.plus")
+                        } else {
+                            Label("\(links.count != 0 ? "\(links.count) Links" : "")", systemImage: "link.badge.plus")
+                        }
+                    }
+                    .buttonStyle(.bordered)
+                    .clipShape(RoundedRectangle(cornerRadius: 100.00))
+                }
+                .padding([.bottom, .leading, .trailing])
                 
                 Button {} label: {
                     Label("Generate Quiz", systemImage: "paperplane")
@@ -87,6 +93,7 @@ struct ContentView: View {
                         .clipShape(RoundedRectangle(cornerRadius: 15))
                 }
                 .padding(.horizontal)
+                Spacer()
             }
             .navigationTitle("ElonMigo")
             .toolbar {
@@ -153,7 +160,30 @@ struct ContentView: View {
                         } header: {
                             Text("API Key")
                         } footer: {
-                            Text("**Reminder:** Never share API keys.")
+                            Text("Grab one from [makersuite.google.com](https://makersuite.google.com/app/apikey)\n**Reminder: Never share API keys.**")
+                        }
+                        
+                        Section {
+                            Picker("Preferred Model", selection: $selectedOption) {
+                                ForEach(options, id: \.self) { option in
+                                    HStack {
+                                        if option == "Gemini 1.5 Flash" {
+                                            Label(" \(option)", systemImage: "bolt.fill")
+                                        } else {
+                                            Label(" \(option)", systemImage: "brain.head.profile")
+                                        }
+                                    }
+                                }
+                            }
+                        } header: {
+                            Text("Choose Model")
+                        } footer: {
+                            
+                            if selectedOption == "Gemini 1.5 Flash" {
+                                Text("You will receive a **faster response** but not necessarily a smarter, more accurate quiz.")
+                            } else {
+                                Text("You will receive a **smarter response** but not necessarily a in a short amount of time.")
+                            }
                         }
                         
                         Section("Privacy") {
