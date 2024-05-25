073a9067d6e2f23a096f027f560de211d1f26ca3
Aaron <0d1y6g0w@duck.com>
2024-05-25 12:27:23 -0700

    Enable Gemini AI

--- ElonMigo/ElonMigoApp.swift
+++ ElonMigo/ElonMigoApp.swift
@@ -11,9 +11,9 @@ import SwiftUI
 struct ElonMigoApp: App {
     @AppStorage("apiKey") var apiKey = ""
     
-//    init() {
-//        GeminiAPI.`init`(with: apiKey)
-//    }
+    init() {
+        GeminiAPI.`init`(with: apiKey)
+    }
     
     var body: some Scene {
         WindowGroup {
