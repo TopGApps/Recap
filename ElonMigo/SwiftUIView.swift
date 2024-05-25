3c5259d3283ec77c138565bfa4c6ab39c3906900
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2024-05-25 09:25:24 -0700

    New App Icon and SwiftUIView = Question view

---
+++ ElonMigo/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ElonMigo
+//
+//  Created by Vaibhav Satishkumar on 5/25/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
