4d5b3d585f8c976e10a73a8195eb3db3bbc1b2bd
Vaibhav Satishkumar <78756662+Visual-Studio-Coder@users.noreply.github.com>
2024-08-05 14:28:08 -0700

    Initial Commit

---
+++ Recap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recap
+//
+//  Created by Vaibhav Satishkumar on 8/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recap/RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapApp.swift
+//  Recap
+//
+//  Created by Vaibhav Satishkumar on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
